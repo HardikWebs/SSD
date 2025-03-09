aa04310eb81a7e9c53f09c75a5510186aa94624c
Hardik <hardikshukla_it21a9_26@dtu.ac.in>
2025-03-09 14:45:04 +0530

    len

--- ssd/data/build.py
+++ ssd/data/build.py
@@ -19,7 +19,7 @@ class BatchCollator:
 
         list_targets = transposed_batch[1]
         print('is_train', self.is_train)
-        print('type(list_targets)', type(list_targets))
+        print('len(list_targets)', len(list_targets))
         print('list_targets', list_targets)
         targets = Container(
             {key: default_collate([d[key] for d in list_targets]) for key in list_targets[0]}
