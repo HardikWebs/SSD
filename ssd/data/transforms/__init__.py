1cd9078e9c6517f67c5c03d19329c5ee5a059c58
Hardik <hardikshukla_it21a9_26@dtu.ac.in>
2025-03-09 12:42:53 +0530

    preprocessing

--- ssd/data/transforms/__init__.py
+++ ssd/data/transforms/__init__.py
@@ -18,6 +18,11 @@ def build_transforms(cfg, is_train=True):
         ]
     else:
         transform = [
+            ConvertFromInts(),
+            PhotometricDistort(),
+            Expand(cfg.INPUT.PIXEL_MEAN),
+            RandomSampleCrop(),
+            RandomMirror(),
             Resize(cfg.INPUT.IMAGE_SIZE),
             SubtractMeans(cfg.INPUT.PIXEL_MEAN),
             ToTensor()
