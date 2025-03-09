81697c74872c7752108de1b1e018cfee416afbd6
Hardik <hardikshukla_it21a9_26@dtu.ac.in>
2025-03-09 15:06:12 +0530

    c

--- ssd/data/build.py
+++ ssd/data/build.py
@@ -21,8 +21,8 @@ class BatchCollator:
         print('is_train', self.is_train)
         print('len(list_targets)', len(list_targets))
         for target in list_targets:
-            for key, value in target.items():
-                print(len(value))
+            print(len(target["boxes"]))
+            print(len(target["labels"]))
 
         print('list_targets', list_targets)
         targets = Container(
