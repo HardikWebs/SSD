9e06448388fb24554a612be256223ecff3134939
Hardik <hardikshukla_it21a9_26@dtu.ac.in>
2025-03-09 15:31:55 +0530

    f

--- ssd/data/build.py
+++ ssd/data/build.py
@@ -57,7 +57,7 @@ class BatchCollator:
 
 def make_data_loader(cfg, is_train=True, distributed=False, max_iter=None, start_iter=0):
     train_transform = build_transforms(cfg, is_train=is_train)
-    target_transform = build_target_transform(cfg) if is_train else None
+    target_transform = build_target_transform(cfg) # if is_train else None
     dataset_list = cfg.DATASETS.TRAIN if is_train else cfg.DATASETS.TEST
     datasets = build_dataset(dataset_list, transform=train_transform, target_transform=target_transform, is_train=is_train)
 
